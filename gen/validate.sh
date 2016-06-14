1d003f455b2eb0b616cf96f8abeed8fabb7443fe
Toby Sullivan <toby.sullivan@teespring.com>
2016-06-13 18:51:32 -0700

    Print wallet address during validation

--- gen/validate.sh
+++ gen/validate.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-echo "WARNING: This script only validates your password."
-echo "It does not check that the private key is valid for your wallet."
-
 echo "Encrypted key: (Press CTRL-D when done)"
 ENC_KEY=`cat`
 
-echo "$ENC_KEY" | openssl enc -aes-256-cbc -d -salt -a > /dev/null
+PRIV_KEY=`echo "$ENC_KEY" | openssl enc -aes-256-cbc -d -salt -a`
 RES=$?
 
 if [ "$RES" == "0" ]
 then
-		echo "Password is valid"
+		echo "SUCCESS! Password is valid."
+		WALLET=`echo "$PRIV_KEY" | node get-wallet-from-private-key.js`
+		echo "The associated wallet is $WALLET"
 else
 		echo "FAIL! Password doesn't match"
 		exit 1
